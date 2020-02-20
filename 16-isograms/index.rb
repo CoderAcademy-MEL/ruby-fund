d5f6482c344a38f30676b4a35e1fd2fae2b158a7
Edward Deam <edwardcrdeam@gmail.com>
2020-02-20 23:00:33 +1100

    update question text

--- 16-isograms/index.rb
+++ 16-isograms/index.rb
@@ -1,4 +1,5 @@
-# An isogram is a word that has no repeating letters, consecutive or non-consecutive.
-# Implement a function that determines whether a string that contains only letters is an isogram.
+# An isogram is a word that has no repeating letters, consecutive or non-consecutive. (https://en.wikipedia.org/wiki/Isogram)
+# Implement a method that determines whether a string (that contains only letters) is an isogram.
+# This method should be called `isogram?`, take a single string as input and return a boolean value (true or false).
 # Assume that an empty string is an isogram.
 # Ignore letter case. e.g. isogram?('moOse') should return false.
