a59330f1abf9ee9f1e5cd887d9596aeefb22bbe6
Edward Deam <edwardcrdeam@gmail.com>
2020-02-20 22:53:19 +1100

    isogram challenge

---
+++ 16-isograms/index.rb
@@ -0,0 +1,4 @@
+# An isogram is a word that has no repeating letters, consecutive or non-consecutive.
+# Implement a function that determines whether a string that contains only letters is an isogram.
+# Assume that an empty string is an isogram.
+# Ignore letter case. e.g. isogram?('moOse') should return false.
