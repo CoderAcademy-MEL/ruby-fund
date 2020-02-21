394454fddda390604b45a3c69527998beff9437b
Edward Deam <edwardcrdeam@gmail.com>
2020-02-21 16:23:22 +1100

    high low number challenge

---
+++ 17-high-low/index.rb
@@ -0,0 +1,8 @@
+# In this challenge you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+# Create a method called high_low that takes a single argument, a string of space separated numbers eg. '2 4 5'
+# All numbers in the string are valid Integers, and can be positive or negative.
+# There will always be at least one number in the input string.
+# The Output should be a String.
+# The Output should be structured as the high number a space and the low number
+# e.g. high_low('1 2 -3 4 5') => '5 -3'
