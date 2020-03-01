88038b0bd20d675b37b7581b43b9f592b4073a69
Edward Deam <edwardcrdeam@gmail.com>
2020-03-02 10:49:17 +1100

    problem setup

---
+++ 19-word-wars/word_wars.rb
@@ -0,0 +1,7 @@
+# Word Enemies
+# Words are going into battle, hurling letters from one to the other. Your
+# method called `battle` should take in two words (strings) as arguments. The 
+# letters that are the same in each word cancel each other out. The remaining 
+# letters have the value of their position in the alphabet (a=1, b=2..). The word
+# with the highest value wins!
+# Return which word won the war
