33a49c3acef2e9b355d6f92a97aec80eb0f43443
Edward Deam <edwardcrdeam@gmail.com>
2020-03-02 13:32:37 +1100

    add test for challenge

---
+++ 19-word-wars/tests.rb
@@ -0,0 +1,31 @@
+require_relative 'word_wars'
+
+describe '#battle' do
+  context 'when properly defined' do
+    it 'be defined' do
+      expect(defined?(battle)).to eq('method')
+    end
+
+    it 'takes two arguments' do
+      expect(method(:battle).parameters.length).to eq(2)
+    end
+
+    it 'returns one of the inputed strings' do
+      expect(battle('hello', 'world')).to eql('hello').or eql('world')
+    end
+  end
+
+  context 'test cases' do
+    it 'returns "world" with inputs "hello" and "world"' do
+      expect(battle('hello', 'world')).to eql('world')
+    end
+
+    it 'returns "alligator" with inputs "alligator" and "elephant' do
+      expect(battle('alligator', 'elephant')).to eql('alligator')
+    end
+
+    it 'returns "conviviality" with inputs "indubitably" and "conviviality"' do
+      expect(battle('indubitably', 'conviviality')).to eql('conviviality')
+    end
+  end
+end
--- 19-word-wars/word_wars.rb
+++ 19-word-wars/word_wars.rb
@@ -1,7 +1,9 @@
 # Word Enemies
 # Words are going into battle, hurling letters from one to the other. Your
-# method called `battle` should take in two words (strings) as arguments. The 
-# letters that are the same in each word cancel each other out. The remaining 
+# method called `battle` should take in two words (strings) as arguments. The
+# letters that are the same in each word cancel each other out. The remaining
 # letters have the value of their position in the alphabet (a=1, b=2..). The word
 # with the highest value wins!
 # Return which word won the war
+# You can assume that the two inputs will be valid, lowercase, non empty strings
+# and that there will be no ties.
