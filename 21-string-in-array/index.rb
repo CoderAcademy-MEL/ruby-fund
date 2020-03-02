8cfd49c76a672e2677d112ba0b2b00a6b4fca841
Harrison Malone <harrison.malone2@gmail.com>
2020-03-02 14:50:54 +1100

    added more challenges without tests

---
+++ 21-string-in-array/index.rb
@@ -0,0 +1,1 @@
+# Write a method that takes two arguments, an array and a string. If the string is not in the array it returns nil, and if it does contain the string, it returns the index of the string within the array. You will use this in the following question. 
---
+++ 24-word-enemies/index.rb
@@ -0,0 +1,3 @@
+# *Word Enemies*
+# Words are going into battle, hurling letters from one to the other. Your function takes in two words (strings) as arguments. The letters that are the same in each word cancel each other out. The letters have the value of their position in the alphabet (a=1, b=2..)
+# Output which word won the war.
